f1fcd46c46f0f4f8991b1c8dd34eb0a8dbc4a0be
Nanda Sree <nandasree3404@gmail.com>
2024-04-25 01:13:27 +0530

    done

---
+++ routes/register.js
@@ -0,0 +1,13 @@
+// const newData=[{
+//     Fullname:'nan',
+//     Bloodgroup:'o+ve',
+//     Email:'nandu@gmail.com',
+//     Mobilenumber:'12345',
+//     State:'ap',
+//     District:'knl',
+//     City:'knl',
+//     Availability:'available',
+//     Last6months_Donations:'yes'
+//   }
+// ]
+// module.exports=newData
